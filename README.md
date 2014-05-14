04ec278a871904be8ca9b6e2ccdfb4b6a1f434ef
Patrick Steele-Idem <pnidem@gmail.com>
2014-05-14 12:03:31 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+warmup
+-----------
+
+Simple module to warmup a server application (such as an Express app) by hitting server URLs and performing various tasks. This module also allows a worker to be warmed up before it is added to a cluster.
+
+# Installation
+
+```
+npm install warmup
+```
+
+# Usage
+
+```javascript
+var warmup = require('warmup');
+warmup(app, tasks, callback);
+warmup(app, tasks, options, callback);
+```
+
+Simple example of warming up an Express server application:
+
+```javascript
+var warmup = require('warmup');
+var express = require('express');
+
+var app = express();
+
+// ...
+
+warmup(
+    app,
+    [
+        '/foo', // A URL to hit to warmup the server
+        '/bar', // A URL to hit to warmup the server
+        function myFunc(callback) { // A custom warmup task
+            var port = this.port; // The warmup port is there if you need it
+            callback();
+        }
+    ],
+    function(err) {
+        app.listen(8080);
+    });
+```
+
+The following options are supported:
+
+* __timeout__ - Timeout for each task (defaults to 10s)
+* __warmupPort__ - The warmup port to use (defaults to a random port in the range of [10,000-50,000])
+
+
+The `warmup` module works by starting the app on a random HTTP port. This allows the application to be started without accepting traffic. By default, a random port is picked.
+
+NOTES:
+
+* All of the tasks are executed in parallel
+* The default timeout is 10s
---
+++ test/test.js
@@ -0,0 +1,64 @@
+'use strict';
+var chai = require('chai');
+chai.Assertion.includeStack = true;
+require('chai').should();
+var expect = require('chai').expect;
+var warmup = require('../');
+
+var express = require('express');
+
+describe('warmup' , function() {
+
+    beforeEach(function(done) {
+        done();
+    });
+
+    it('should warmup a simple app', function(done) {
+        
+
+        var app = express();
+
+        var completed = {};
+
+        app.get('/foo', function(req, res) {
+            setTimeout(function() {
+                res.end('foo');
+                completed.foo = true;
+            }, 200);
+        });
+
+        app.get('/bar', function(req, res) {
+            setTimeout(function() {
+                res.end('bar');
+                completed.bar = true;
+            }, 200);
+        });
+
+        var port = null;
+
+        warmup(
+            app,
+            [
+                '/foo',
+                '/bar',
+                function(callback) {
+                    port = this.port;
+                    setTimeout(function() {
+                        completed.func = true;
+                        callback();
+                    }, 200);
+                }
+            ],
+            function(err) {
+                if (err) {
+                    return done(err);
+                }
+
+                expect(completed.foo).to.equal(true);
+                expect(completed.bar).to.equal(true);
+                expect(completed.func).to.equal(true);
+                done();
+            });
+    });
+});
+
